6634c0ce421b0f45715140e5b74c2b0e89eb336e
DougTheDruid <51171051+DougTheDruid@users.noreply.github.com>
2021-09-09 07:29:10 -0500

    update formatting

--- offset_finder.py
+++ offset_finder.py
@@ -2,21 +2,20 @@
 Script that is responsible for looking through our SDK and determining
 all of the offsets we specify automatically. Can that pass the output file
 into the SoT ESP Framework
-
 @Author https://github.com/DougTheDruid
 @Source https://github.com/DougTheDruid/SoT-Python-Offset-Finder
 @Framework https://github.com/DougTheDruid/SoT-ESP-Framework
-
 For community support, please contact me on Discord: DougTheDruid#2784
 """
 import json
+import re
 
 
 ATHENA = "SDK\\Athena_Classes.h"
 ENGINE = "SDK\\Engine_Classes.h"
 ATHENA_AI = "SDK\\AthenaAI_Classes.h"
-ENGINESTRUCT = "SDK\\Engine_Structs.h"
-ATHENASTRUCT = "SDK\\Athena_structs.h"
+ENGINE_STRUCT = "SDK\\Engine_Structs.h"
+ATHENA_STRUCT = "SDK\\Athena_structs.h"
 
 
 def get_offset(file_name, title, memory_object):
@@ -37,9 +36,21 @@ def get_offset(file_name, title, memory_object):
             if title == line.replace("\n", ""):
                 past_header = True
             if past_header:
-                if memory_object in line:
+                #  This regex looks for a leading space, our property name,
+                #  and a trailing semicolon
+                if re.search(f"\s{memory_object};", line):
                     offset = line.split("// ")[1].split("(")[0]
                     return int(offset, 0)
+            if past_header and line == '\n':  # reached end of property list
+                raise ValueError(
+                    f"Unable to find property {memory_object} in {title[3:]}."
+                    f" Check your property name is correct."
+                )
+
+        raise ValueError(
+            f'Unable to find header {title} in {file_name}. '
+            'Check your header line is correct.'
+        )
 
 
 if __name__ == '__main__':
@@ -58,7 +69,7 @@ if __name__ == '__main__':
         "UWorld.PersistentLevel": get_offset(ENGINE, "// Class Engine.World", "PersistentLevel"),
         "APlayerController.CameraManager": get_offset(ENGINE, "// Class Engine.PlayerController", "PlayerCameraManager"),
         "APlayerCameraManager.CameraCache": get_offset(ENGINE, "// Class Engine.PlayerCameraManager", "CameraCache"),
-        "FCameraCacheEntry.FMinimalViewInfo": get_offset(ENGINESTRUCT, "// ScriptStruct Engine.CameraCacheEntry", "POV"),
+        "FCameraCacheEntry.FMinimalViewInfo": get_offset(ENGINE_STRUCT, "// ScriptStruct Engine.CameraCacheEntry", "POV"),
     }
     with open("offsets.json", "w+") as outfile:
         outfile.write(json.dumps(output, indent=2, sort_keys=True))
